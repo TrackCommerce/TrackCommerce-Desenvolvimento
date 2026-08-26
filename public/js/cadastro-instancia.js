4be6a6667806c992fe93bab94a3643baea6ae549
gustavoLimaAraujo <gustavo.araujo@sptech.school>
2026-08-26 09:08:09 -0300

    adicionando validação e lógica para encaminhar o usuário para a terceira tela

--- public/js/cadastro-instancia.js
+++ public/js/cadastro-instancia.js
@@ -38,8 +38,8 @@ let primeiraEtapaCadastro = `
                 </div>
             </section>
     
-            <section class="sessao-formulario">
-                <h2 class="titulo-formulario">
+            <section class="sessao-card">
+                <h2 class="titulo-sessao">
                     Identificação do Servidor
                 </h2>
     
@@ -135,8 +135,8 @@ let segundaEtapaCadastro = `
                 </div>
             </section>
     
-            <section class="sessao-formulario">
-                <h2 class="titulo-formulario">
+            <section class="sessao-card">
+                <h2 class="titulo-sessao">
                     Componentes Físicos
                 </h2>
     
@@ -155,16 +155,16 @@ let segundaEtapaCadastro = `
 
                         <div class="opcoes-monitoramento">
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Monitorar Processador</label>
+                                <input type="checkbox" id="checkbox_monitorar_cpu">
+                                <label for="checkbox_monitorar_cpu">Monitorar Processador</label>
                             </div>
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Porcentagem de Uso</label>
+                                <input type="checkbox" id="checkbox_porcentagem_cpu">
+                                <label for="checkbox_porcentagem_cpu">Porcentagem de Uso</label>
                             </div>
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Frequência do Processador</label>
+                                <input type="checkbox" id="checkbox_frequencia_cpu">
+                                <label for="checkbox_frequencia_cpu">Frequência do Processador</label>
                             </div>
                         </div>
                     </div>
@@ -178,16 +178,16 @@ let segundaEtapaCadastro = `
 
                         <div class="opcoes-monitoramento">
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Monitorar Armazenamento</label>
+                                <input type="checkbox" id="checkbox_monitorar_disco">
+                                <label for="checkbox_monitorar_disco">Monitorar Armazenamento</label>
                             </div>
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Porcentagem de Uso</label>
+                                <input type="checkbox" id="checkbox_porcentagem_disco">
+                                <label for="checkbox_porcentagem_disco">Porcentagem de Uso</label>
                             </div>
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Quantidade de GB (Gigas) Livre</label>
+                                <input type="checkbox" id="checkbox_disco_livre">
+                                <label for="checkbox_disco_livre">Quantidade de GB (Gigas) Livre</label>
                             </div>
                         </div>                        
                     </div>
@@ -201,16 +201,16 @@ let segundaEtapaCadastro = `
 
                         <div class="opcoes-monitoramento">
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Monitorar Memória</label>
+                                <input type="checkbox" id="checkbox_monitorar_ram">
+                                <label for="checkbox_monitorar_ram">Monitorar Memória</label>
                             </div>
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Porcentagem de Uso</label>
+                                <input type="checkbox" id="checkbox_porcentagem_ram">
+                                <label for="checkbox_porcentagem_ram">Porcentagem de Uso</label>
                             </div>
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Quantidade de GB (Gigas) Livre</label>
+                                <input type="checkbox" id="checkbox_ram_livre">
+                                <label for="checkbox_ram_livre">Quantidade de GB (Gigas) Livre</label>
                             </div>
                         </div>                        
                     </div>
@@ -225,12 +225,12 @@ let segundaEtapaCadastro = `
 
                          <div class="opcoes-monitoramento">
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Monitorar Rede</label>
+                                <input type="checkbox" id="checkbox_monitorar_rede">
+                                <label for="checkbox_monitorar_rede">Monitorar Rede</label>
                             </div>
                             <div class="opcao">
-                                <input type="checkbox" name="" id="">
-                                <label for="">Monitorar Latência da Rede</label>
+                                <input type="checkbox" id="checkbox_latencia_rede">
+                                <label for="checkbox_latencia_rede">Monitorar Latência da Rede</label>
                             </div>
                         </div>                        
                     </div>
@@ -241,26 +241,86 @@ let segundaEtapaCadastro = `
                 <button class="botao cancelar" onclick="trocaTela(primeiraEtapaCadastro)">
                     Voltar
                 </button>
-                <button class="botao avancar">
+                <button class="botao avancar" onclick="transicaoTerceiraEtapa()">
                     Avançar
                 </button>
             </section>
 `
 
-let popUp = `
-        <div class="pop_up">
-            <div class="botao-saida" onclick="fecharPopUp(containerPopUp)">
-                <button>X</button>
-            </div>
-            <div class="texto_alerta">
-                <p>
-                    Você não preencheu todos os campos! 
+let terceiraEtapaCadastro = `
+            <section class="sessao-titulo">
+                <h1 class="titulo">
+                    Cadastro de novo servidor
+                </h1>
+                <p class="sub-titulo">
+                    Complete os campos abaixo para cadastrar o servidor para ser monitorado!
                 </p>
-                <p>
-                    Preencha eles corretamente!
-                </p>
-            </div>
-        </div>
+            </section>
+    
+            <section class="sessao-processo">
+                <div class="processo-completo">
+                    <div class="processo">
+                        <h2 class="numero">
+                            1
+        
+                        </h2>
+                        <h2 class="titulo-processo">
+                            Identificação
+                        </h2>
+                    </div>
+                    <div class="processo">
+                        <h2 class="numero">
+                            2
+        
+                        </h2>
+                        <h2 class="titulo-processo">
+                            Componentes
+                        </h2>
+                    </div>
+                    <div class="processo">
+                        <h2 class="numero">
+                            3
+                        </h2>
+                        <h2 class="titulo-processo">
+                            Paramêtros
+                        </h2>
+                    </div>
+                </div>
+            </section>
+    
+            <section class="alerta-parametros">
+                <div class="lembrete-parametro">
+                    <p>
+                        Lembrete!!!
+                    </p>
+                    <p>
+                        Caso nenhum paramêtro seja específicado, todos os componentes emitiram alertas a partir de 80% de uso (ou similar, dependendo da escolha de monitoramento)
+                    </p>
+                </div>
+            </section>
+
+            <section class="sessao-card">
+                <h2 class="titulo-sessao">
+                    Paramêtros
+                </h2>
+    
+                <div class="caixa-explicacao">
+                    Escolha o limite de porcentagem de uso para que o sistema solte um alerta.
+                </div>
+                
+                <div class="caixa_parametros">
+                    <div id="especificacoes_parametros"></div>
+                </div>
+            </section>
+    
+            <section class="botoes">
+                <button class="botao cancelar" onclick="trocaTela(segundaEtapaCadastro)">
+                    Voltar
+                </button>
+                <button class="botao avancar" onclick="transicaoTerceiraEtapa()">
+                    Avançar
+                </button>
+            </section>
 `
 
 // Pegando o element com a classe chamada "container-cadastro"
@@ -270,7 +330,7 @@ let containerPopUp = document.querySelector('.container_pop_up');
 
 // Incializando a aplicação com a primeira etapa do cadastro
 containerCadastro.innerHTML = primeiraEtapaCadastro;
-containerPopUp.innerHTML = popUp;
+containerPopUp.innerHTML = criarPopUpBasico("Você não preencheu todos os campos! <br> Preencha eles corretamente!");
 
 function transicaoSegundaEtapa() {
     let iptApelidoServidor = ipt_apelido_servidor.value;
@@ -279,13 +339,69 @@ function transicaoSegundaEtapa() {
     if(iptApelidoServidor == "" || iptIdentificadorServidor == ""){
         containerPopUp.style.display = "block";
     } else {
+        fecharPopUp(containerPopUp);
         trocaTela(segundaEtapaCadastro);
+    }
+}
+
+function transicaoTerceiraEtapa() {
+    let checkboxMonitorarCpu = document.querySelector("#checkbox_monitorar_cpu");
+    let checkboxPorcetangemCpu = document.querySelector("#checkbox_porcentagem_cpu");
+    let checkboxFrequenciaCpu = document.querySelector("#checkbox_frequencia_cpu");
+    let checkboxMonitorarDisco = document.querySelector("#checkbox_monitorar_disco");
+    let checkboxPorcetagemDisco = document.querySelector("#checkbox_porcentagem_disco");
+    let checkboxDiscoLivre = document.querySelector("#checkbox_disco_livre");
+    let checkboxMonitorarRam = document.querySelector("#checkbox_monitorar_ram");
+    let checkboxPorcentagemRam = document.querySelector("#checkbox_porcentagem_ram");
+    let checkboxRamLivre = document.querySelector("#checkbox_ram_livre");
+    let checkboxMonitorarRede = document.querySelector("#checkbox_monitorar_rede");
+    let checkboxLatenciaRede = document.querySelector("#checkbox_latencia_rede");
+
+    if(
+        (checkboxPorcetangemCpu.checked || checkboxFrequenciaCpu.checked) && !checkboxMonitorarCpu.checked ||
+        (checkboxPorcetagemDisco.checked || checkboxDiscoLivre.checked) && !checkboxMonitorarDisco.checked ||
+        (checkboxPorcentagemRam.checked || checkboxRamLivre.checked) && !checkboxMonitorarRam.checked ||
+        checkboxLatenciaRede.checked && !checkboxMonitorarRede.checked 
+    ){
+        containerPopUp.style.display = "block";
+        containerPopUp.innerHTML = criarPopUpBasico("Você está tentando monitorar porcentagens ou relacionados de componentes sem selecionar os componentes! <br> (Dica: Lembre-se sempre de escolher primeiro que vai monitorar e depois as suas funcionalidades");
+    } else if(!checkboxMonitorarCpu.checked && !checkboxMonitorarDisco.checked && !checkboxMonitorarRam.checked && !checkboxMonitorarRede.checked) {
+        containerPopUp.style.display = "block";
+        containerPopUp.innerHTML = criarPopUpBasico("Selecione pelo menos um componente para ser monitorado!");    
+    } else {
+        if(
+            checkboxMonitorarCpu.checked && !(checkboxPorcetangemCpu.checked || checkboxFrequenciaCpu.checked) ||
+            checkboxMonitorarDisco.checked && !(checkboxPorcetagemDisco.checked || checkboxDiscoLivre.checked) ||
+            checkboxMonitorarRam.checked && !(checkboxPorcentagemRam.checked || checkboxRamLivre.checked) ||
+            checkboxLatenciaRede.checked && !checkboxMonitorarRede.checked 
+        ) {
+            containerPopUp.style.display = "block";
+            containerPopUp.innerHTML = criarPopUpBasico("Você selecionou apenas o componente que quer monitorar! Selecione alguns atributos para monitorar esse componente");        
+        } else {
+            fecharPopUp(containerPopUp);
+            trocaTela(terceiraEtapaCadastro);        
+        }
     }
 }
 
 function trocaTela(telaDestino) {
     containerCadastro.innerHTML = telaDestino;
 }
+
+function criarPopUpBasico(textoAlerta){
+    return `
+            <div class="pop_up">
+                <div class="botao-saida" onclick="fecharPopUp(containerPopUp)">
+                    <button>X</button>
+                </div>
+                <div class="texto_alerta">
+                    <p>
+                        ${textoAlerta}
+                    </p>
+                </div>
+            </div>
+    `
+} 
 
 function fecharPopUp(classeDoPopUp) {
     classeDoPopUp.style.display = "none";
