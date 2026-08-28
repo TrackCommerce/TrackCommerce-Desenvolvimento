168c1cdfc26ea71e7f0b075aa3f150db5f1498a3
gustavoLimaAraujo <gustavo.araujo@sptech.school>
2026-08-28 20:55:03 -0300

    iniciando a criação do pop-up de edição

---
+++ public/js/lista-instancia.js
@@ -0,0 +1,9 @@
+// Ao incrementar com o banco de dados, penso em utilizar como paramêtro o próprio ID da máquina virtual
+// A partir da criação de todos os card, já irei adicionar o ID de cada instância como paramêtro
+function popUpAtualizarInstancia() {
+    let containerMainLista = document.querySelector('.botao-editar');
+
+    containerMainLista.style.cursor = 'default'
+
+    
+}
