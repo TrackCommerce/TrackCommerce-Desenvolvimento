5f8a966e59a2c884795110483cdcf0d9e4fc1a36
RafaelNaleto <rafael.naletofo@sptech.school>
2026-08-31 20:57:02 -0300

    Adicionando rotas no cadastro de funcionários

---
+++ src/controller/usuarioController.js
@@ -0,0 +1,44 @@
+var usuarioModel = require("../models/usuarioModel");
+
+function cadastrar(req, res) {
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var contato = req.body.contatoServer;
+    var cargo = req.body.cargoServer;
+    var permissoes = req.body.permissoesServer;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (contato == undefined) {
+        res.status(400).send("Seu contato está undefined!");
+    } else if (cargo == undefined) {
+        res.status(400).send("Cargo está undefined!");
+    } else if (permissoes == undefined) {
+        res.status(400).send("Permissões está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(nome, email, contato, cargo, permissoes)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    cadastrar
+}
