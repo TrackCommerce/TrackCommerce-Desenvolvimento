d725cde726637f94b433e50a5223d1bb2f45081f
EmanuellyMel <emanuelly.melo@sptech.school>
2026-09-07 22:29:08 -0300

    feat: funcionalidades da pagina permissoes (back-end)

--- src/controllers/permissoesController.js
+++ src/controllers/permissoesController.js
@@ -15,6 +15,78 @@ function buscarCargos(req, res){
     });
 }
 
+function buscarPermissoes(req, res){
+    
+    let idCargo = req.params.idCargo;
+
+    permissoesModel.buscarPermissoes(idCargo).then(function (resultado){
+        res.status(200).json(resultado);
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as permissões.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function editarPermissoes(req, res){
+
+    let idCargo = req.params.idCargo;
+    let permissoes = req.body.permissoes;
+
+    permissoesModel.editarPermissoes(idCargo, permissoes).then(function (resultado){
+        res.json(resultado);
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao editar as permissões.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function deletarCargo(req, res) {
+    var idCargo = req.params.idCargo;
+
+    permissoesModel.deletarCargo(idCargo)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao deletar o cargo: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function adicionarCargo(req, res) {
+
+    var nome = req.body.nomeServer;
+    var experiencia = req.body.experienciaServer;
+    var permissoes = req.body.permissoesServer;
+
+        permissoesModel.adicionarCargo(nome, experiencia, permissoes)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+
 module.exports = {
-    buscarCargos
+    buscarCargos,
+    buscarPermissoes,
+    editarPermissoes,
+    deletarCargo,
+    adicionarCargo
 }
--- src/routes/permissoes.js
+++ src/routes/permissoes.js
@@ -7,4 +7,19 @@ router.get("/cargos", function (req, res){
     permissoesController.buscarCargos(req, res);
 });
 
+router.get("/buscarPermissoes/:idCargo", function (req, res){
+    permissoesController.buscarPermissoes(req, res);
+});
+
+router.post("/editarPermissoes/:idCargo", function (req, res){
+    permissoesController.editarPermissoes(req, res);
+});
+
+router.delete("/deletarCargo/:idCargo", function (req, res){
+    permissoesController.deletarCargo(req, res);
+});
+
+router.post("/adicionarCargo", function (req, res){
+    permissoesController.adicionarCargo(req, res);
+});
 module.exports = router;
